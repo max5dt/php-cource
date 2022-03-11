438d6e81cdd4e25a81ea4dd46e241e23d9c0c108
PLESK\mkrasikov <mkrasikov@plesk.com>
2022-03-11 10:19:44 +0300

    Ex1 - binarySum

--- binarySum.php
+++ binarySum.php
@@ -1,7 +1,9 @@
 <?php
+declare(strict_types=1);
 
-function binarySum($num1, $num2) {
-    return (decbin(bindec($num1) + bindec($num2)));
+function binarySum(string $num1, string $num2): string
+{
+    return decbin(bindec($num1) + bindec($num2));
 }
 
 print_r(binarySum('10', '1') . "\n");
