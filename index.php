3381df86f759405bfd1a69a652d67666727612ea
PLESK\mkrasikov <mkrasikov@plesk.com>
2022-04-04 11:44:13 +0300

    Some cosmetics in index.php

--- index.php
+++ index.php
@@ -9,9 +9,9 @@ $ex = new \MyApp\Tasks\FizzBuzz();
 try {
     echo ($ex->fizzBuzz(1.4, 16)) . PHP_EOL;
 } catch (\TypeError $e) {
-    echo "Type mismatch". PHP_EOL;
-}  catch (\InvalidArgumentException $e) {
+    echo "Type mismatch" . PHP_EOL;
+} catch (\InvalidArgumentException $e) {
     echo $e->getMessage();
 } catch (\Throwable $e) {
-    echo 'Global exception'. $e->getMessage(). PHP_EOL;
+    echo 'Global exception' . $e->getMessage() . PHP_EOL;
 }
