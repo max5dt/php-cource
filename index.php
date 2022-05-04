2c4b4e9b2b9ee1521b7205d74b4107117dd94ea2
PLESK\mkrasikov <mkrasikov@plesk.com>
2022-05-04 09:19:49 +0300

    Steps to implement Logger

--- index.php
+++ index.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 
 require __DIR__ . '/vendor/autoload.php';
 
-$ex = new \MyApp\Tasks\FizzBuzz();
+//error_reporting(0);
+
+$stdoutLogger = new \MyApp\Logger\StdoutLogger();
+$fileLogger = new \MyApp\Logger\FileLogger(__DIR__ . '/log.txt');
+
+(new \MyApp\Tasks\BinarySum($fileLogger))->binarySum('1', '');
+
+(new \MyApp\Tasks\BinarySum($fileLogger))->binarySum('1', '2');
+
+exit;
+
+$ex = new \MyApp\Tasks\BinarySum();
 
 try {
-    echo ($ex->fizzBuzz(10, 1)) . PHP_EOL;
+    echo ($ex->binarySum("10", "1")) . PHP_EOL;
 } catch (\TypeError $e) {
     echo "Type mismatch" . PHP_EOL;
 } catch (\InvalidArgumentException $e) {
--- src/Tasks/BinarySum.php
+++ src/Tasks/BinarySum.php
@@ -4,17 +4,35 @@ declare(strict_types=1);
 
 namespace MyApp\Tasks;
 
+use MyApp\Logger\FileLogger;
+use MyApp\Logger\LoggerInterface;
+use MyApp\Logger\StdoutLogger;
+
 class BinarySum
 {
+
+    private $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
     public function binarySum(string $num1, string $num2): string
     {
         if ($num1 === "" || $num2 === "") {
+            $this->logger->err('Log - Empty input');
             throw new \InvalidArgumentException('Error: Empty input');
         }
         if (((preg_match('~^[01]+$~', $num1)) === 0) || ((preg_match('~^[01]+$~', $num2)) === 0)) {
+            $this->logger->err('Log - Not a binary number');
             throw  new \InvalidArgumentException('Error: Not a binary number');
         }
 
-        return decbin(bindec($num1) + bindec($num2));
+        $result = decbin(bindec($num1) + bindec($num2));
+
+        $this->logger->info("Sum of " . $num1 . " and " . $num2 . " is " . $result);
+
+        return $result;
     }
 }
